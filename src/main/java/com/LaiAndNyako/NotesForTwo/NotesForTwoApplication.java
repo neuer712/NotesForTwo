ae2945190e9f0cc3706442bc7d4310880c04c651
Wei.Lai(Future) <neuer712@163.com>
2020-05-25 22:45:36 +0800

    Upload the base backend project of NoteForTwo

---
+++ src/main/java/com/LaiAndNyako/NotesForTwo/NotesForTwoApplication.java
@@ -0,0 +1,13 @@
+package com.LaiAndNyako.NotesForTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesForTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesForTwoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/LaiAndNyako/NotesForTwo/pages/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.LaiAndNyako.NotesForTwo.pages.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = "/index")
+	public String MainPage() {
+		//System.out.println("tttttest");
+		return "main/index";
+	}
+}
